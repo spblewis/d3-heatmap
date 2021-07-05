91d90497040c89dad99e0ef9766954285a110083
spblewis <spblewis@gmail.com>
2021-07-05 18:27:57 -0500

    Working on webpack config didn't plan on that originally, but as good a time as any to learn it...

---
+++ index.js
@@ -0,0 +1,9 @@
+import * as d3 from 'd3';
+
+const svg = d3.select('main')
+  .append('svg')
+  .attr('height', '500px')
+  .attr('width', '800px')
+  .style('background-color', 'green');
+
+svg.select('');
