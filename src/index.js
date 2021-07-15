6f9e9feb6c6f56735032c1e868de5c7953d2a2de
spblewis <spblewis@gmail.com>
2021-07-15 16:37:42 -0500

    Fetching data, looking at the shape of the data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import * as d3 from 'd3';
 
+const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+
 const svg = d3.select('main')
   .append('svg')
   .attr('height', '500px')
   .attr('width', '800px')
   .style('background-color', 'green');
 
-svg.select('');
+
+d3.json(source).then((data) => {
+
+  document.getElementById('dummy').innerHTML = JSON.stringify(data);
+
+});
+
+  svg.select('');
