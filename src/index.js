f851da057d44184debca76b56b9b0e5410d7f842
spblewis <spblewis@gmail.com>
2021-07-18 18:25:27 -0500

    Applied a rudimentary color scale

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
 
 const height = 500;
-const width = 800;
+const width = 1200;
 const padding = 30;
 
 const svg = d3.select('main')
@@ -30,7 +30,7 @@ d3.json(source).then((data) => {
     .attr('id', 'x-axis')
     .call(xAxis);
 
-  //y axis
+  // y axis
   const yScale = d3.scaleLinear();
   yScale.domain([0, 12])
     .range([height - padding, padding]);
@@ -42,6 +42,17 @@ d3.json(source).then((data) => {
     .attr('id', 'y-axis')
     .call(yAxis);
 
+  // color gradient
+
+  const colorScale = d3.scaleQuantize()
+  colorScale.domain([
+    d3.min(dataset, (d) => d.variance),
+    d3.max(dataset, (d) => d.variance)
+  ])
+    .range(['blue', 'light-blue', 'yellow', 'pink', 'red', 'brown']);
+
+  // cell data
+
   const cells = svg.selectAll('rect')
     .data(dataset)
     .enter()
@@ -49,12 +60,12 @@ d3.json(source).then((data) => {
     .attr('class', 'cell')
     .attr('data-month', (d) => d.month)
     .attr('data-year', (d) => d.year)
-    .attr('data-temp', (d) => d.temperature)
-    .attr('height', '2px')
-    .attr('width', '2px')
-    .attr('x', (d) => (d.year - 1753) + 30)
-    .attr('y', (d) => (d.month * 3) + 100)
-    .attr('fill', 'blue');
+    .attr('data-temp', (d) => d.variance)
+    .attr('height', (height - padding) / 12 - 1)
+    .attr('width', 3)
+    .attr('x', (d) => xScale(d.year))
+    .attr('y', (d) => yScale(d.month))
+    .attr('fill', (d) => colorScale(d.variance));
 
 });
 
