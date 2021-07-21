5b14a209194a2bc16009d616e5225e52c32b5405
spblewis <spblewis@gmail.com>
2021-07-20 19:31:24 -0500

    Heading, description, month names

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import * as d3 from 'd3';
 
+// Declare unchanging constants.
 const source = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
-
 const height = 500;
 const width = 1200;
-const padding = 30;
+const padding = 75;
+
 const colors = [
   "#313695",
   "#4575b4",
@@ -19,11 +20,39 @@ const colors = [
   "#a50026"
 ];
 
+// Helper function to get month names from their numbers (0 indexed)
+const getTheMonth = (n) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+
+  return months[n];
+}
+
 const svg = d3.select('main')
   .append('svg')
   .attr('height', height)
   .attr('width', width);
 
+const tooltip = d3.select('main')
+  .append('div')
+  .style('height', 200)
+  .style('width', 200)
+  .style('background-color', 'gray')
+  .style('display', 'none');
+
+// Get the data
 d3.json(source).then((data) => {
   const dataset = data.monthlyVariance;
   console.log(data);
@@ -45,10 +74,11 @@ d3.json(source).then((data) => {
 
   // y axis
   const yScale = d3.scaleLinear();
-  yScale.domain([0, 12])
+  yScale.domain([0, 11])
     .range([height - padding, padding]);
 
-  const yAxis = d3.axisLeft(yScale);
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat(getTheMonth);
   
   svg.append('g')
     .attr('transform', `translate(${padding}, 0)`)
@@ -69,7 +99,7 @@ d3.json(source).then((data) => {
     .enter()
     .append('rect')
     .attr('class', 'cell')
-    .attr('data-month', (d) => d.month)
+    .attr('data-month', (d) => d.month - 1)
     .attr('data-year', (d) => d.year)
     .attr('data-temp', (d) => d.variance)
     .attr('height', (height - padding) / 12 - 1)
@@ -77,5 +107,8 @@ d3.json(source).then((data) => {
     .attr('x', (d) => xScale(d.year))
     .attr('y', (d) => yScale(d.month))
     .attr('fill', (d) => colorScale(d.variance));
+
+  // tooltip
+  
 
 });
